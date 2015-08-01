9ef9c776fc7475dd69d573ce68cc8bb6c7514030
Alex Gittens <gittens@icsi.berkeley.edu>
2015-08-01 23:37:07 +0000

    moved stuff around

---
+++ src/runcsvtoparquet.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+CURDIR=`dirname $(realpath $0)`
+LOGDIR=$CURDIR/../eventLogs
+JARNAME=$1
+INSOURCE=$2
+OUTDEST=$3
+LOGNAME=$CURDIR/../CSVToParquetConversion.log
+
+spark-submit --verbose \
+	--master yarn \
+	--driver-memory 5G \
+	--executor-memory 5G \
+	--conf spark.eventLog.enable=true \
+	--conf spark.eventLog.dir=$LOGDIR \
+	--jars $JARNAME \
+	--class org.apache.spark.climate.CSVToParquet \
+	$JARNAME \
+	$INSOURCE $OUTDEST \
+	2>&1 | tee $LOGNAME
