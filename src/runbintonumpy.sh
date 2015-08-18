ff5490a16f212d424be6f76f9ebec3f0de58a78b
Alex Gittens <gittens@icsi.berkeley.edu>
2015-08-18 01:19:19 +0000

    completed visualization of temporal and spatial eofs, and writing to netcdf format

--- src/runbintonumpy.sh
+++ src/runbintonumpy.sh
@@ -5,12 +5,7 @@ CURDIR=`dirname $(realpath $0)`
 WORKINGDIR=$CURDIR/..
 JARFILE=$1
 SPARKHOME=/opt/Spark
-SPARKHOME=/root/spark
-SPARKHOME=/root/spark
-SPARKHOME=/root/spark
-SPARKHOME=/root/spark
-SPARKHOME=/root/spark
-SPARKHOME=/root/spark
+#SPARKHOME=/root/spark
 
 INBIN=$WORKINGDIR/data/eofs-centerOverAllObservations-20.bin
 OUTCSV=$WORKINGDIR/data
@@ -23,3 +18,9 @@ $SPARKHOME/bin/spark-submit --master local --verbose \
    --driver-memory 2G \
    --class org.apache.spark.mllib.linalg.distributed.ConvertDump \
    $JARFILE $INBIN $OUTCSV
+
+hdfs dfs -get $ROWINDICES_HDFS $OUTCSV
+cd $OUTCSV/recordDateMapping
+gunzip *
+cat part* > ../csvrow-to-gribdate-mapping
+hdfs dfs -get $COLINDICES_HDFS ../parquetcol-to-csvrow-mapping
