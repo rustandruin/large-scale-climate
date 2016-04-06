1b7eb228afef3ce5508c64631f9f893b18b59560
Evan Racah <eracah@lbl.gov>
2016-04-06 00:56:01 -0700

    edited some submit scripts

--- run_locally.sh
+++ run_locally.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -l
 
-./local-submit.sh ./../target/scala-2.10/large-scale-climate-assembly-0.0.1.jar ~/projects/spark-test/h5spark/file_list.txt 150 192 centerOverAllObservations 20 . exact
+src/local-submit.sh ./target/scala-2.10/pca_with_h5_and_timings-assembly-0.0.1.jar  ~/projects/spark-test/h5spark/file_list.txt 150 192 centerOverAllObservations 20 . exact
